f3be62d555f4e9d43c9363f8c3250b7848f9cf7c
lanycsq <shanqiong118@163.com>
2021-03-09 13:44:57 +0800

    deploy

---
+++ precache-manifest.ce496b4c32a2247514ea3fc328da7214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6906563846d4f5d6c3",
+    "url": "/morney-website/css/app.eee42617.css"
+  },
+  {
+    "revision": "24f92f2d55c73ef68632f768f340ff1a",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "2a6906563846d4f5d6c3",
+    "url": "/morney-website/js/app.a1519a23.js"
+  },
+  {
+    "revision": "707cdde4c0704b23cb8c",
+    "url": "/morney-website/js/chunk-vendors.941d12e3.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
